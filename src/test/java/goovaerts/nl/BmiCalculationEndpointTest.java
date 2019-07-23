b8b3f9523d710364bd86b6413a54eb1c85d5eb06
Caroline Goovaerts <caroline@goovaerts.nl>
2019-07-23 12:05:03 +0200

    Initial code with failing test

---
+++ src/test/java/goovaerts/nl/BmiCalculationEndpointTest.java
@@ -0,0 +1,12 @@
+package goovaerts.nl;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class BmiCalculationEndpointTest {
+
+    @Test
+    void x() {
+        Assertions.fail("not yet implemented");
+    }
+}
