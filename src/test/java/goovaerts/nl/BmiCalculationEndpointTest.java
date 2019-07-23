cffca72d9c74b7c3d73d6e54f3f22057252867b9
Caroline Goovaerts <caroline@goovaerts.nl>
2019-07-23 12:38:46 +0200

    Add initial BMI request object

--- src/test/java/goovaerts/nl/BmiCalculationEndpointTest.java
+++ src/test/java/goovaerts/nl/BmiCalculationEndpointTest.java
@@ -1,15 +1,20 @@
 package goovaerts.nl;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class BmiCalculationEndpointTest {
 
-    @Test
-    void shouldReturnBmiResponse() {
-        BmiCalculationEndpoint endpoint = new BmiCalculationEndpoint();
-        BmiResponse response = endpoint.calculate(new Object());
+    private BmiCalculationEndpoint endpoint;
 
-        Assertions.assertNotNull(response);
+    @BeforeEach
+    void setUp() {
+        endpoint = new BmiCalculationEndpoint();
+    }
+
+    @Test
+    void shouldReturnBmiResponseForBmiRequest() {
+        Assertions.assertNotNull(endpoint.calculate(new BmiRequest()));
     }
 }
