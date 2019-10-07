b3fcff6652ed1e863a65f25693e2168cbe5aaba9
Caroline Goovaerts <caroline@goovaerts.nl>
2019-10-07 14:49:57 +0200

    Comment on missing code

--- src/main/java/nl/goovaerts/orm/PersonRepository.java
+++ src/main/java/nl/goovaerts/orm/PersonRepository.java
@@ -4,4 +4,6 @@ import nl.goovaerts.domain.Person;
 import org.springframework.data.repository.CrudRepository;
 
 public interface PersonRepository extends CrudRepository<Person, Long> {
+
+    // Spring Data JPA magic
 }
